18dd86195359831ee8eacaafdcb53abd96c5e339
Mathias Rönnlund <mathias.ronnlund@gmail.com>
2017-03-25 20:55:21 +0200

    https

--- Hemma.Web/Controllers/TemperaturController.cs
+++ Hemma.Web/Controllers/TemperaturController.cs
@@ -17,7 +17,7 @@ namespace Hemma.Web.Controllers
         ActionResult Temperatur(string id)
         {
             ViewBag.ChannelId = ConfigurationManager.AppSettings["Temperature:" + id.ToLower() + ":ChannelId"];
-            ViewBag.ApiReadKey = ConfigurationManager.AppSettings["Temperature:" + id.ToLower() + ":ApiReadyKey"];
+            ViewBag.ApiReadKey = ConfigurationManager.AppSettings["Temperature:" + id.ToLower() + ":ApiReadKey"];
             ViewBag.DataInterval = ConfigurationManager.AppSettings["Temperature:" + id.ToLower() + ":DataInterval"];
             return View("Index");
 
